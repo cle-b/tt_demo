67342b7be62c04a5145e0253b8a3155c3496e822
cle-b <cle@tictac.pm>
2019-02-10 18:53:00 +0100

    add setup configuration

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    desc = fh.read()
+    long_description = desc[desc.find("# tt_demo"):]  # remove bagde
+
+with open("tt_demo/requirements.txt", "r") as fh:
+    install_requires = fh.read().splitlines()
+
+setuptools.setup(
+    name="tt_demo",
+    version="0.0.1",
+    author="cle-b",
+    author_email="cle@tictac.pm",
+    description="My implementation of the Toucan Toco back test for technical interview.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cle-b/tt_demo",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Apache Software License'
+    ],
+)
