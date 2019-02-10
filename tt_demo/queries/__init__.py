076702e4aea463eb8d2a99bcdf3e4161a461944f
cle-b <cle@tictac.pm>
2019-02-10 18:54:09 +0100

    first commit - create queries

---
+++ tt_demo/queries/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*
+
+from .services import create_queries_from_cson
+
+__all__ = ["create_queries_from_cson"]
---
+++ tt_demo/tt_demo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*
+import os
+
+import connexion
+from flask_pymongo import PyMongo
+
+
+def create_app(app_name, mongodb_uri, port=9090, debug=False):
+    options = {"swagger_ui": debug}
+
+    app = connexion.FlaskApp(app_name,
+                             port=port,
+                             specification_dir='openapi/',
+                             options=options,
+                             debug=debug)  # fix as FLASK_DEBUG flag not works...
+
+    app.add_api('swagger-ttdemo.yaml')
+
+    app.app.config["MONGO_URI"] = mongodb_uri
+    app.app.mongo = PyMongo(app.app)
+
+    return app
+
+
+if __name__ == '__main__':
+    debug = os.getenv("FLASK_DEBUG", "0") == "1"
+    mongdb_uri = os.getenv("TT_MONGO_URI", "mongodb://localhost:27017/mytest")
+
+    app = create_app(__name__,
+                     mongodb_uri=mongdb_uri,
+                     debug=debug)
+    app.run()
